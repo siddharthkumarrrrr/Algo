61f30640d304782e260e117a2588d7d2b708519d
siddharth kumar <siddharthkumarrrrr@gmail.com>
2025-06-11 13:59:48 +0530

    backend integreation

--- src/App.jsx
+++ src/App.jsx
@@ -77,10 +77,10 @@ function App() {
           {/* Instruction text below the button */}
           {showInstructions && (
             <p style={{ marginTop: '10px', fontSize: '14px', color: 'gray' }}>
-              Open the advanced option under **"App Settings"** and **ON "Developer Options"**.
               Click 
-              <FontAwesomeIcon icon={faCog} style={{ marginRight: '5px' }} /> 
-              beside your name. Copy your Discord ID.
+              <FontAwesomeIcon icon={faCog} style={{ marginRight: '5px' }} />  beside your name.
+               Open the advanced option under **"App Settings"** and **ON "Developer Options"**.
+              Copy your Discord ID by clicking on userName;
 
               {/* Corrected clickable link */}
               <a href="https://discord.com" target="_blank" rel="noopener noreferrer">
